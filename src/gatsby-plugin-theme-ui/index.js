28c1b3141e88e566a4fa762d7feb7b3d96c3ed7a
Simon White <simon.white@investisdigital.com>
2022-10-25 19:48:27 +0100

    Adding global Theme UI file for Gatsby.

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,7 +1,18 @@
 const theme = {
   config: {
-    initialColorModeName: 'light',
+    initialColorModeName: "light",
     useColorSchemeMediaQuery: false,
+  },
+  images: {
+    headerLogo: {
+      width: 40,
+      height: 35,
+    },
+    avatar: {
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+    },
   },
   colors: {
     text: "#404040",
@@ -9,134 +20,120 @@ const theme = {
     primary: "#F78181",
     secondary: "#9a9a9a",
     muted: "#f7f7f7",
-    gray: '#9a9a9a',
+    gray: "#9a9a9a",
     lightGray: "#ccc",
     tertiary: "#0B7A75",
     highlight: "rgba(120, 120, 120, 0.25)",
     modes: {
       dark: {
-        text: '#f4f4f4',
-        background: '#264653',
+        text: "#f4f4f4",
+        background: "#264653",
         muted: "#2E5564",
         lightGray: "#333",
-        primary: '#97D8C4',
+        primary: "#97D8C4",
         secondary: "#306",
         tertiary: "#FFC857",
         highlight: "rgba(0, 0, 0, 0.1)",
-      }
-    }
+      },
+    },
   },
   sizes: {
     container: 1140,
+    sidebar: 256,
   },
-  space: [
-    0,
-    5,
-    10,
-    15,
-    30,
-    60,
-    90,
-    120
-  ],
+  space: [0, 5, 10, 15, 30, 60, 90, 120],
   fonts: {
-    "body": "Montserrat, sans-serif",
-    "heading": "Montserrat, sans-serif",
-    "monospace": "Menlo, monospace"
+    body: "Montserrat, sans-serif",
+    heading: "Montserrat, sans-serif",
+    monospace: "Menlo, monospace",
   },
-  fontSizes: [
-    12,
-    14,
-    16,
-    20,
-    24,
-    32,
-    48,
-    64,
-    96
-  ],
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
   buttons: {
     primary: {
-      color: 'background',
-      bg: 'primary',
+      color: "background",
+      bg: "primary",
       cursor: "pointer",
-      '&:hover': {
-        bg: 'text',
-      }
+      "&:hover": {
+        bg: "text",
+      },
     },
     secondary: {
-      color: 'text',
-      bg: 'muted',
+      color: "text",
+      bg: "muted",
       cursor: "pointer",
-      '&:hover': {
-        bg: 'secondary',
-        color: 'background',
+      "&:hover": {
+        bg: "secondary",
+        color: "background",
       },
       borderRadius: 0,
     },
     tertiary: {
-      color: 'background',
-      bg: 'tertiary',
+      color: "background",
+      bg: "tertiary",
       cursor: "pointer",
-      '&:hover': {
-        bg: 'text',
-      }
-    }
+      "&:hover": {
+        bg: "text",
+      },
+    },
+    menu: {
+      backgroundColor: "red",
+      display: ["initial", "none"],
+    },
   },
   cards: {
     primary: {
-      bg: 'background',
+      bg: "background",
       padding: 4,
       borderRadius: 6,
       boxShadow: t => `0 0 5px ${t.colors.highlight}`,
     },
     article: {
-      bg: 'background',
+      bg: "background",
     },
   },
   links: {
     nav: {
       px: 2,
       py: 1,
-      textTransform: 'uppercase',
-      letterSpacing: '0.2em',
+      textTransform: "uppercase",
+      letterSpacing: "0.2em",
     },
     plain: {
-      color: 'text',
-      textDecoration: 'none',
-      '&:hover': {
-        color: 'primary',
-      }
-    }
+      color: "text",
+      textDecoration: "none",
+      "&:hover": {
+        color: "primary",
+      },
+    },
   },
   forms: {
     label: {
       fontSize: 1,
-      color: 'primary',
-      fontWeight: 'bold'
+      color: "primary",
+      fontWeight: "bold",
     },
     input: {
-      backgroundColor: 'background',
+      backgroundColor: "background",
       mb: 4,
-      borderColor: 'gray',
-      '&:focus': {
-        borderColor: 'primary',
+      borderColor: "gray",
+      "&:focus": {
+        borderColor: "primary",
         boxShadow: t => `0 0 0 2px ${t.colors.primary}`,
-        outline: 'none',
+        outline: "none",
       },
     },
     select: {
-      borderColor: 'gray',
-    }
+      borderColor: "gray",
+    },
   },
   fontWeights: {
     body: 300,
     heading: 400,
-    bold: 400
+    bold: 400,
   },
   lineHeights: {
     body: 1.625,
-    heading: 1.25
+    heading: 1.25,
   },
   text: {
     heading: {
@@ -147,55 +144,55 @@ const theme = {
       a: {
         color: "inherit",
         textDecoration: "none",
-        '&:hover': {
+        "&:hover": {
           textDecoration: "underline",
-        }
-      }
+        },
+      },
     },
     large: {
-      fontSize: 5
-    }
+      fontSize: 5,
+    },
   },
   styles: {
     root: {
-      "fontFamily": "body",
-      "lineHeight": "body",
-      "fontWeight": "body"
+      fontFamily: "body",
+      lineHeight: "body",
+      fontWeight: "body",
     },
     a: {
-      "color": "primary"
+      color: "primary",
     },
     pre: {
-      "fontFamily": "monospace",
-      "overflowX": "auto",
-      "code": {
-        "color": "inherit"
-      }
+      fontFamily: "monospace",
+      overflowX: "auto",
+      code: {
+        color: "inherit",
+      },
     },
-    "code": {
-      "fontFamily": "monospace",
-      "fontSize": "inherit"
+    code: {
+      fontFamily: "monospace",
+      fontSize: "inherit",
     },
-    "table": {
-      "width": "100%",
-      "borderCollapse": "separate",
-      "borderSpacing": 0
+    table: {
+      width: "100%",
+      borderCollapse: "separate",
+      borderSpacing: 0,
     },
-    "th": {
-      "textAlign": "left",
-      "borderBottomStyle": "solid"
+    th: {
+      textAlign: "left",
+      borderBottomStyle: "solid",
     },
-    "td": {
-      "textAlign": "left",
-      "borderBottomStyle": "solid"
+    td: {
+      textAlign: "left",
+      borderBottomStyle: "solid",
     },
-    "img": {
-      "maxWidth": "100%"
+    img: {
+      maxWidth: "100%",
     },
     hr: {
-      borderColor: 'lightGray'
-    }
-  }
-}
+      borderColor: "lightGray",
+    },
+  },
+};
 
-export default theme
+export default theme;
