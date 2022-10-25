e33e674a9837e42653971c71cd31d299d7ca894b
Simon White <simon.white@investisdigital.com>
2022-10-25 21:00:47 +0100

    First pass at defining the social component.

---
+++ src/components/social.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Container } from "theme-ui";
+
+const Social = () => {
+  return (
+    <Container
+      sx={{
+        mb: "4",
+        mt: "4",
+      }}
+    >
+    </Container>
+  );
+};
+
+export default Social;
