c6871d8faead3810330adb7cfddda7e4ba496fd5
Simon White <simon.white@investisdigital.com>
2022-10-28 17:37:58 +0100

    Adding the social component.

--- src/components/social.js
+++ src/components/social.js
@@ -1,6 +1,8 @@
-import * as React from "react";
-import { Container, Box } from "theme-ui";
+import React from "react";
+import { Container, Box, Grid, jsx } from "theme-ui";
 import { SiDrupal, SiLinkedin, SiCodepen, SiGithub } from "react-icons/si";
+
+/** @jsx jsx */
 
 const Social = () => {
   return (
@@ -10,18 +12,48 @@ const Social = () => {
         mt: "4",
       }}
     >
-      <Box>
-        <SiDrupal />
-      </Box>
-      <Box>
-        <SiLinkedin />
-      </Box>
-      <Box>
-        <SiCodepen />
-      </Box>
-      <Box>
-        <SiGithub />
-      </Box>
+      <Grid gap={2} columns={[2, null, 4]} sx={{ justifyItems: "center" }}>
+        <Box className="gradient-border">
+          <a
+            href="https://www.drupal.org/u/whitewebs"
+            target={"_blank"}
+            rel={"noreferrer"}
+            sx={{ display: "block" }}
+          >
+            <SiDrupal sx={{ width: 50, height: 50 }} />
+          </a>
+        </Box>
+        <Box className="gradient-border">
+          <a
+            href="https://www.linkedin.com/in/simonmwhite"
+            target={"_blank"}
+            rel={"noreferrer"}
+            sx={{ display: "block" }}
+          >
+            <SiLinkedin sx={{ width: 50, height: 50 }} />
+          </a>
+        </Box>
+        <Box className="gradient-border">
+          <a
+            href="https://codepen.io/simonmwhite"
+            target={"_blank"}
+            rel={"noreferrer"}
+            sx={{ display: "block" }}
+          >
+            <SiCodepen sx={{ width: 50, height: 50 }} />
+          </a>
+        </Box>
+        <Box className="gradient-border">
+          <a
+            href="https://github.com/simonmwhite"
+            target={"_blank"}
+            rel={"noreferrer"}
+            sx={{ display: "block" }}
+          >
+            <SiGithub sx={{ width: 50, height: 50 }} />
+          </a>
+        </Box>
+      </Grid>
     </Container>
   );
 };
